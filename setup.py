598f07110e69e82cb5ea864a7c88f97895cab960
Gerad Coles <gerad@prairiedogbrewing.ca>
2019-07-06 14:05:40 -0600

    bump version for changes

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 # pylint: disable=no-name-in-module,F0401,W0232,C0111,R0201
 
+
 def readme():
     "Returns the contents of the README.rst file"
     with open("README.rst") as f:
         return f.read()
 
+
 setup(
     name='py-futrli',
-    version="0.1",
+    version="0.2",
     description='Python client for uploading non-financial data to Futrli',
     long_description=readme(),
     author='Prairie Dog Brewing CANADA Inc',
@@ -17,7 +19,7 @@ setup(
     packages=find_packages(),
     install_requires=[],
     scripts=[
-        #'bin/foo',
+        # 'bin/foo',
     ],
     test_suite="nose.collector",
 )
